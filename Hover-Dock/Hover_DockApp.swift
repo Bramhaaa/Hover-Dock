151bbf064a6aa1f10fffa113d339979396c756a4
Bramha Bajannavar <altbramhabajannavar@gmail.com>
2025-12-13 14:34:29 +0530

    Initial Commit

---
+++ Hover-Dock/Hover_DockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hover_DockApp.swift
+//  Hover-Dock
+//
+//  Created by Bramha Bajannavar on 13/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Hover_DockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hover-DockTests/Hover_DockTests.swift
@@ -0,0 +1,17 @@
+//
+//  Hover_DockTests.swift
+//  Hover-DockTests
+//
+//  Created by Bramha Bajannavar on 13/12/25.
+//
+
+import Testing
+@testable import Hover_Dock
+
+struct Hover_DockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
