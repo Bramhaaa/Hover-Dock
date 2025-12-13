9a70d599fde5a66d22d6de57d1b48f9d72613082
Bramha Bajannavar <altbramhabajannavar@gmail.com>
2025-12-13 18:21:00 +0530

    initial macos app setup

--- Hover-Dock/Hover_DockApp.swift
+++ Hover-Dock/Hover_DockApp.swift
@@ -5,10 +5,18 @@
 //  Created by Bramha Bajannavar on 13/12/25.
 //
 
+
 import SwiftUI
+import ApplicationServices
 
 @main
 struct Hover_DockApp: App {
+
+    init() {
+        let trusted = AXIsProcessTrusted()
+        print("Accessibility trusted:", trusted)
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
